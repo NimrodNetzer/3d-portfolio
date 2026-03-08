dce5a2703a709ff1670343cd3e95acf1451005f2
NimrodNetzer <nimrodnetzer@gmail.com>
2026-03-08 11:26:15 +0200

    change object's scaling

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,8 +21,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.35 : 0.4}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.3 : 0.35} 
+        position={isMobile ? [0, -3, -3.0] : [0, -3.25, -2.5]} 
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
